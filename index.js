315f37bc12581c823e24cfac137dd645af47b964
Sean Martin <sean@seanmartinstudio.com>
2021-11-04 13:36:53 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution in this file!
+//#1
+function distanceFromHqInBlocks(pickupBlock) {
+    const hqBlock = 42;
+    let distance = pickupBlock - hqBlock;
+    return distance;
+}
+console.log(distanceFromHqInBlocks(43));
+
+
