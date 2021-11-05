ac9ae7dc6ae0d43b81918c2243ebab77cdc92328
Sean Martin <sean@seanmartinstudio.com>
2021-11-05 15:08:59 -0700

    commit tree 67771ac3628cd9bf0c79fb66d575184cb3eba018

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
 //#1
 function distanceFromHqInBlocks(pickupBlock) {
     const hqBlock = 42;
-    let distance = pickupBlock - hqBlock;
-    return distance;
+    if (pickupBlock > 42) {
+        return pickupBlock - hqBlock
+    } else {
+        return hqBlock - pickupBlock
+    }
 }
 console.log(distanceFromHqInBlocks(43));
 
 
+//#2
+function distanceFromHqInFeet(pickupBlock) {
+   return distanceFromHqInBlocks(pickupBlock) * 264
+
+}
+console.log(distanceFromHqInFeet(43));
+
+//#3
+function distanceTravelledInFeet(start, destination) {
+    const blockLengthInFeet = 264;
+    if (start > destination) {
+        return (start - destination) * 264
+    } else {
+        return (destination - start) * 264
+    }
+}
+console.log(distanceTravelledInFeet(43, 48));
+
+//#4
+function calculatesFarePrice(start, destination) {
+   //let price = 0;
+   if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0
+   } else if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02
+   } else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+       return 25
+   } else {
+       return "cannot travel that far"
+   }
+}
+calculatesFarePrice(43, 48);
